bc3479bc6006c52941abf64c3f19aa5f19cf0789
rukhsana1827 <rukhsana1827asif@gmail.com>
2026-03-31 12:45:01 +0530

    changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,8 +3,7 @@ import { RouterOutlet, RouterLinkWithHref, RouterModule } from '@angular/router'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,
-    RouterLinkWithHref,
+  imports: [RouterOutlet, RouterLinkWithHref,
     RouterModule
   ],
   templateUrl: './app.html',
@@ -12,4 +11,14 @@ import { RouterOutlet, RouterLinkWithHref, RouterModule } from '@angular/router'
 })
 export class App {
   protected readonly title = signal('Bookkeeping');
+
+  closeNavbar() {
+    const navbar = document.getElementById('ftco-nav');
+    if (navbar && navbar.classList.contains('show')) {
+      // Small timeout ensures the route change begins before the menu disappears
+      setTimeout(() => {
+        navbar.classList.remove('show');
+      }, 100);
+    }
+  }
 }
